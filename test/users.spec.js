4addddbbe0a84675f6a70264b175b6ddaea89d47
user-dob <user-dob@yande.ua>
2017-02-06 18:00:09 +0200

    fix test

--- test/users.spec.js
+++ test/users.spec.js
@@ -3,9 +3,3 @@ describe('Users factory', function() {
         expect(2 + 2).toEqual(4);
     });
 });
-
-describe('Users factory 2', function() {
-    it('has a dummy spec to test 2 + 3', function() {
-        expect(2 + 3).toEqual(5);
-    });
-});
