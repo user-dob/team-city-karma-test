473cab77790c7b5a208dd1cb6eb06c6cf52b4a77
user-dob <user-dob@yande.ua>
2017-02-06 16:30:26 +0200

    add failed test

--- test/users.spec.js
+++ test/users.spec.js
@@ -3,3 +3,9 @@ describe('Users factory', function() {
         expect(2 + 2).toEqual(4);
     });
 });
+
+describe('Users factory 2', function() {
+    it('has a dummy spec to test 2 + 1', function() {
+        expect(2 + 1).toEqual(4);
+    });
+});
