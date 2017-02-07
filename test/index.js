60b4d8b9dd4cd51afcc7d59d0ef16fb1efc3930f
user-dob <user-dob@yandex.ua>
2017-02-07 16:59:17 +0200

    fix

--- test/index.js
+++ test/index.js
@@ -12,11 +12,4 @@ describe('webdriver.io page', function() {
 		var title = browser.getTitle();
 		assert.equal(title, 'WebdriverIO - Selenium 2.0 javascript bindings for nodejs');
 	});
-
-	it('should have the right title - the fancy generator way', function () {
-		browser.url('http://webdriver.io');
-		var title = browser.getTitle();
-		assert.equal(title, 'WebdriverIO - Selenejs');
-	});
-
 });
