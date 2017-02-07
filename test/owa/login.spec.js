25eaaf46f307c2b16a8d2c07d52d75328bceabd8
user-dob <user-dob@yandex.ua>
2017-02-07 19:32:24 +0200

    fix

--- test/owa/login.spec.js
+++ test/owa/login.spec.js
@@ -1,15 +1,23 @@
 var LOGIN = 'VBTestSF365@decadd.onmicrosoft.com'
 var PASSWORD = 'aB123456'
 
-describe('Github', function() {
+describe('outlook.office.com/owa', function() {
 
 	it('Demo test GitHub', function (client) {
 		client
-			.url('https://github.com/nightwatchjs/nightwatch')
+			.url('https://outlook.office.com/owa/')
 			.waitForElementVisible('body', 1000)
-			.assert.title('nightwatchjs/nightwatch · GitHub')
-			.assert.visible('.container h1 strong a')
-			.assert.containsText('.container h1 strong a', 'nightwatch', 'Checking project title is set to nightwatch');
+			.click('#cred_userid_inputtext')
+			.setValue('#cred_userid_inputtext', LOGIN)
+			.click('#cred_password_inputtext')
+			.setValue('#cred_password_inputtext', PASSWORD)
+			.pause(1000)
+			.click('#cred_sign_in_button')
+			.waitForElementVisible('#_ariaId_24', 10000).click('#_ariaId_24')
+			.waitForElementVisible('button._1Fd2vs8lcUZ55PLwMz6tRl', 10000).click('button._1Fd2vs8lcUZ55PLwMz6tRl')
+			.click('.ms-ContextualMenu-list .ms-ContextualMenu-item:nth-child(2)')
+			.pause(10000)
+			.assert.title('GitHub - nightwatchjs/nightwatch: Automated testing and continous integration framework based on node.js and selenium webdriver')
 	});
 
 	after(function(client, done) {
@@ -22,4 +30,4 @@ describe('Github', function() {
 		}
 	});
 
-});
+})
