db2cb99893a23294d3e722da5bce1f18488b3ac6
user-dob <user-dob@yandex.ua>
2017-02-21 21:55:44 +0200

    fix

---
+++ FrontendTests/e2e/add-in/Account-open-in-Salesforce-2/Account-open-in-Salesforce.js
@@ -0,0 +1,24 @@
+mode('Account open relative Contact', function (config) {
+
+	it('Account open relative Contact', function (browser) {
+		var addin = browser.page.addin();
+		var name = 'OneAccountEEE'
+
+		addin
+			.search(name)
+			.assert.containsText('.app_cards .card-info-fullname', name)
+			.click('.app_card_template .relatives_btn_title[data-name="Contact"]')
+			.waitForElementVisible('.app_card_template .card_related_type_menu:not(.closed)', 10000)
+			.assert.visible('.app_card_template .card_related_type_menu:not(.closed) .title_modal')
+			.getText('.app_card_template .card_related_type_menu:not(.closed) .title_modal', function (result) {
+
+				browser
+					.click('.app_card_template .card_related_type_menu:not(.closed)')
+					.waitForRequestCompleted(60000)
+					.assert.containsText('.app_card.opened [data-name="Account.Name"] .text', name)
+					.end()
+
+			})
+	})
+
+})
