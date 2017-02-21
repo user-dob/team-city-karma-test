b1d1d1e2d766b3e2e4002b2811404d0d3da6c8a8
user-dob <user-dob@yandex.ua>
2017-02-21 18:50:20 +0200

    as

--- FrontendTests/e2e/add-in/Account-open-in-Salesforce/Account-open-in-Salesforce.js
+++ FrontendTests/e2e/add-in/Account-open-in-Salesforce/Account-open-in-Salesforce.js
@@ -4,6 +4,19 @@ describe('test', function () {
 		browser
 			.url('http://nightwatchjs.org/')
 			.waitForElementVisible('body', 3000)
+			.assert.title("Nightwatch.js | Node.js powered End-to-End testing framework")
+			.end()
+	})
+
+})
+
+
+describe('test', function () {
+
+	it('test', function (browser) {
+		browser
+			.url('http://nightwatchjs.org/')
+			.waitForElementVisible('body', 3000)
 			.assert.title("Nightwatch.js | Node.js powered End-to-End testing framework-")
 			.end()
 	})
