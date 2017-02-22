69d0df4a257a4e4bb620485b70910b120f61dde9
user-dob <user-dob@yandex.ua>
2017-02-22 12:36:56 +0200

    s

---
+++ FrontendTestFramework/run.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var cmd = require('node-cmd');
+var config = require('./nightwatch.config')
+
+function buildCommandLine(config) {
+	var command = []
+
+	command.push('java -jar')
+
+	var cliArgs = []
+	for(var args in config.cli_args) {
+		cliArgs.push('-D' + args + '=' + config.cli_args[args])
+	}
+
+	command.push(cliArgs.join(' '))
+
+	command.push(config.server_path)
+
+	command.push('-port ' + config.port)
+
+	command.push('-log ' + config.log_path)
+
+	return command.join(' ')
+}
+
+cmd.run(buildCommandLine(config.selenium))
