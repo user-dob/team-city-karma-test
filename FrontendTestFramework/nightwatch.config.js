88b2063cb50e1f4cfecd278ff16cf83b4e98ad13
user-dob <user-dob@yandex.ua>
2017-02-22 12:45:20 +0200

    as

--- FrontendTestFramework/nightwatch.config.js
+++ FrontendTestFramework/nightwatch.config.js
@@ -19,7 +19,7 @@ module.exports = {
 	"selenium" : {
 		"start_process" : false,
 		"server_path" : path.join(__dirname, 'selenium/selenium-server-standalone-3.0.1.jar'),
-		"log_path" : "./log",
+		"log_path" : "./log/",
 		"port" : 4440,
 		"cli_args" : {
 			"webdriver.chrome.driver" : path.join(__dirname, 'selenium/chromedriver.exe'),
